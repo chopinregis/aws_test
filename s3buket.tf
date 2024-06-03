e66979e7f6ff97609930edc63b3f07667b72f260
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-06-03 19:13:54 -0400

    Update s3buket.tf

--- s3buket.tf
+++ s3buket.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
   force_destroy = false
+  acl    = "private"
+  versioning_configuration {
+    status = "Disabled"
+  }
 
   tags = {
     Name        = "My bucket"
