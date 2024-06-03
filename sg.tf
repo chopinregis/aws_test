e3999b41f55f406069a7230d5881fcf76be035f9
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-06-03 19:52:32 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -19,3 +19,30 @@ resource "aws_default_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+resource "aws_security_group" "datasg" {
+  vpc_id = aws_vpc.mainvpc.id
+  name = "vpc-database"
+  description = "Dev VPC DATABASE SECURITY GROUP"
+
+  ingress {
+    description = "Allow port 3306"
+    protocol  = "tcp"
+    from_port = 3306
+    to_port   = 3306
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "vpc-database"
+  }
+
+}
