75648d7e97fb3126ee4f9132b0f869805bb179ad
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-06-03 18:49:33 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,12 +7,9 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+provider "aws" {
+  region = var.aws_region
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESSS_KEY
 }
