7ad10daf487893c8284f6c6d5eabdfbe5bcee2e9
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-06-03 20:04:32 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,42 @@
-data "aws_ami" "ubuntu" {
+resource "aws_vpc" "example" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+data "aws_ami" "amzn-linux-2023-ami" {
   most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["al2023-ami-2023.*-x86_64"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "c6a.2xlarge"
+  subnet_id     = aws_subnet.example.id
+
+  cpu_options {
+    core_count       = 2
+    threads_per_core = 2
+  }
 
   tags = {
-    Name = "HelloWorld"
+    Name = "tf-example"
   }
 }
